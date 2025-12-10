5c0283fb34f0a16dd4f9c396e3265ab28cf3b413
Mrughank <mrugankchaudhary@gmail.com>
2025-12-10 12:04:50 +0530

    Fix cart qty, mobile UI, place order

--- Controller/cartcontroller.js
+++ Controller/cartcontroller.js
@@ -8,53 +8,46 @@ const getCart = (req, res) => {
 
   Cart.findOne({ userId })
     .populate("items.foodId")
-    .then((cart) => {
-      if (!cart) {
-        return res.send({ success: true, cart: { items: [] } });
-      }
+    .then(cart => {
+      if (!cart) return res.send({ success: true, cart: { items: [] } });
       res.send({ success: true, cart });
     })
-    .catch((err) => {
-      res.status(500).send({ success: false, msg: err.message });
-    });
+    .catch(err => res.status(500).send({ success: false, msg: err.message }));
 };
 
 // --------------------- ADD TO CART ---------------------
 const addToCart = (req, res) => {
   const userId = req.user.id;
-  const { foodId, qty = 1 } = req.body;
+  const { foodId } = req.body;
 
   Food.findById(foodId)
-    .then((food) => {
-      if (!food) {
-        return res.status(404).send({ msg: "Food not found" });
-      }
+    .then(food => {
+      if (!food) return res.status(404).send({ msg: "Food not found" });
 
-      Cart.findOne({ userId }).then((cart) => {
-        if (!cart) {
-          cart = new Cart({ userId, items: [] });
+      Cart.findOne({ userId }).then(cart => {
+        if (!cart) cart = new Cart({ userId, items: [] });
+
+        const existingItem = cart.items.find(i => i.foodId.equals(foodId));
+
+        if (existingItem) {
+          existingItem.qty += 1;
+        } else {
+          cart.items.push({
+            itemId: uuidv4(),
+            foodId,
+            sellerId: food.sellerId,
+            qty: 1
+          });
         }
 
-        cart.items.push({
-          itemId: uuidv4(),
-          foodId: foodId,
-          sellerId: food.sellerId,
-          qty: qty,
+        cart.save().then(() => {
+          Cart.findOne({ userId })
+            .populate("items.foodId")
+            .then(updated => res.send({ success: true, cart: updated }));
         });
-
-        cart
-          .save()
-          .then(() => {
-            return Cart.findOne({ userId }).populate("items.foodId");
-          })
-          .then((updatedCart) => {
-            res.send({ success: true, cart: updatedCart });
-          });
       });
     })
-    .catch((err) => {
-      res.status(500).send({ success: false, msg: err.message });
-    });
+    .catch(err => res.status(500).send({ success: false, msg: err.message }));
 };
 
 // --------------------- UPDATE QTY ---------------------
@@ -64,16 +57,11 @@ const updateQty = (req, res) => {
   const { itemId } = req.params;
 
   Cart.findOne({ userId })
-    .then((cart) => {
-      if (!cart) {
-        return res.status(404).send({ msg: "Cart not found" });
-      }
+    .then(cart => {
+      if (!cart) return res.status(404).send({ msg: "Cart not found" });
 
-      const item = cart.items.find((i) => i.itemId === itemId);
-
-      if (!item) {
-        return res.status(404).send({ msg: "Item not found" });
-      }
+      const item = cart.items.find(i => i.itemId === itemId);
+      if (!item) return res.status(404).send({ msg: "Item not found" });
 
       if (type === "inc") item.qty++;
       if (type === "dec" && item.qty > 1) item.qty--;
@@ -81,14 +69,10 @@ const updateQty = (req, res) => {
       cart.save().then(() => {
         Cart.findOne({ userId })
           .populate("items.foodId")
-          .then((updated) => {
-            res.send({ success: true, cart: updated });
-          });
+          .then(updated => res.send({ success: true, cart: updated }));
       });
     })
-    .catch((err) => {
-      res.status(500).send({ success: false, msg: err.message });
-    });
+    .catch(err => res.status(500).send({ success: false, msg: err.message }));
 };
 
 // --------------------- REMOVE ITEM ---------------------
@@ -96,44 +80,32 @@ const removeItem = (req, res) => {
   const userId = req.user.id;
   const itemId = req.params.itemId;
 
-  Cart.findOne({ userId })
-    .then((cart) => {
-      if (!cart)
-        return res.send({ success: true, cart: { items: [] } });
+  Cart.findOne({ userId }).then(cart => {
+    if (!cart) return res.send({ success: true, cart: { items: [] } });
 
-      cart.items = cart.items.filter((i) => i.itemId !== itemId);
+    cart.items = cart.items.filter(i => i.itemId !== itemId);
 
-      cart.save().then(() => {
-        Cart.findOne({ userId })
-          .populate("items.foodId")
-          .then((updated) => {
-            res.send({ success: true, cart: updated });
-          });
-      });
-    })
-    .catch((err) =>
-      res.status(500).send({ success: false, msg: err.message })
-    );
+    cart.save().then(() => {
+      Cart.findOne({ userId })
+        .populate("items.foodId")
+        .then(updated => res.send({ success: true, cart: updated }));
+    });
+  });
 };
 
 // --------------------- CLEAR CART ---------------------
 const clearCart = (req, res) => {
   const userId = req.user.id;
 
-  Cart.findOneAndUpdate({ userId }, { items: [] }, { new: true })
-    .then(() => {
-      res.send({ success: true, cart: { items: [] } });
-    })
-    .catch((err) => {
-      res.status(500).send({ success: false, msg: err.message });
-    });
+  Cart.findOneAndUpdate({ userId }, { items: [] }).then(() => {
+    res.send({ success: true, cart: { items: [] } });
+  });
 };
 
-// --------------------- EXPORT ---------------------
 module.exports = {
   getCart,
   addToCart,
   updateQty,
   removeItem,
-  clearCart,
+  clearCart
 };
--- Routes/cartroutes.js
+++ Routes/cartroutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const {
   addToCart,
   getCart,
@@ -11,19 +10,10 @@ const {
 
 const userAuth = require("../Middleware/authMiddleware");
 
-// ADD TO CART
 router.post("/add", userAuth, addToCart);
-
-// GET USER CART
 router.get("/", userAuth, getCart);
-
-// UPDATE QUANTITY
 router.put("/update/:itemId", userAuth, updateQty);
-
-// REMOVE ITEM
 router.delete("/remove/:itemId", userAuth, removeItem);
-
-// CLEAR CART
 router.delete("/clear", userAuth, clearCart);
 
 module.exports = router;
